2e38413879aa576063bb796b0305b2a54231b281
milam <mila.milstead@gmail.com>
2025-02-14 23:51:44 -0500

    Complete

--- src/main/java/SkillBuilder3.java
+++ src/main/java/SkillBuilder3.java
@@ -11,43 +11,67 @@ public class SkillBuilder3
     private static String repeat(String s, int numOfTimesToRepeat)
     {
         String result = "";
-        // TODO: replace this line with your code
+        for (int i = 0; i < numOfTimesToRepeat; i++) {
+            result += s;
+        }
         return result;
     }
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++) {
+            str += repeat("", i) + "\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++) {
+            str += repeat(" ", height - i);
+            str += repeat("", i) + "\n";
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        // TODO: replace this line with your code
+        int x;
+        for (int i = radius-1; i > -radius; i--) {
+
+            x = (int) (Math.sqrt(Math.pow(radius, 2) - (Math.pow(i, 2))));
+
+            str += repeat(" ", radius - x);
+            str += repeat("", 2*x) + "\n";
+        }
         return str;
     }
 
     public static long sumOfDivisors(long number) {
         long sum = 0;
-        // TODO: replace this line with your code
+        for (int i = 1; i < number; i++){
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
         return sum;
     }
 
     public static boolean isPerfect(long number){
-        return false;
+        return sumOfDivisors(number) == number;
     }
 
     public static long sumOfDivisorsUsingWhile(long number){
         long sum = 0;
-        // TODO: replace this line with your code
+        int i = 1;
+        while (i < number) {
+            if (number % i == 0) {
+                sum += i;
+            }
+            i++;
+        }
         return sum;
     }
 }
